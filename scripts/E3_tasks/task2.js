0f5c631cc6b9d6cf4512b58910c30c8dd25db5d1
Anton Fedosov <fedosov-toxa@mail.ru>
2024-03-29 10:40:54 +0300

    task 2 E3 done

--- scripts/E3_tasks/task2.js
+++ scripts/E3_tasks/task2.js
@@ -1,32 +1,25 @@
-
-
-function checkPrimeNum(num){
-    if (num <= 1000){
-        if (num <= 1){
-            console.log('Число является четным!');
+function checkPrimeNum(num) {
+    if (num <= 1000) {
+        if ((num == 1) || (num == 0)) {
+            console.log(`Число ${num} не является ни простым, ни составным!`);
             return 0;
         }
-        if (num == 2){
-            console.log('Число является простым!');
-            return 0;
-        }
-        if (num % 2 == 0){
-            console.log('Число является четным!');
-            return 0;
-        }
-        for (let i=3; i<(num**0.5+1); i+=2){
-            console.log(i)
+        let counter = 0;
+        for (let i = 2; i < Math.floor(num / 2 + 1); i++) {
             if (num % i == 0) {
-                console.log('Число является простым!');
-                return 0;
+                counter += 1;
             }
-            console.log('Число является нечетным!');
-            return 0;
         }
-    }
-    else {
+        if (counter <= 0) {
+            console.log(`Число ${num} простое!`);
+        } else {
+            console.log(`Число ${num} не является простым!`);
+        }
+
+
+    } else {
         console.log('Введенные Вами данные неверны!')
     }
 }
 
-checkPrimeNum(333)
+checkPrimeNum(28)
